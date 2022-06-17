615bddbd2acb0d528f8b080bbfb4be1f187f9742
kelvin <kelvin.aditya@weekendinc.com>
2022-06-18 01:59:28 +0700

    ya

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div className='App'>apa aja kok</div>;
+  return <div className='App'>apa aja kok ya</div>;
 }
 
 export default App;
