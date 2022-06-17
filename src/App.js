2a5d8c1626f9201812ad7585d2faaf15ca377d19
kelvin <kelvin.aditya@weekendinc.com>
2022-06-18 02:25:03 +0700

    update nih

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div className='App'>apa aja kok ya</div>;
+  return <div className='App'>apa aja kok ya yes</div>;
 }
 
 export default App;
 //test
-//tambah
